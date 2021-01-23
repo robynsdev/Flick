e9cb266a8ddc896e7a192dc93ae866fc86ee7138
robyn <65057146+robynsdev@users.noreply.github.com>
2021-01-24 00:40:52 +1100

    created Movie controller and added Movie def index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  scope '/api' do
+    get '/movies', to: 'movies#index'
+    post '/movies', to: 'movies#create'
+    get '/movies/:id', to: 'movies#show'
+    delete '/movies/:id', to: 'movies#destroy'
+  end
 end
+
