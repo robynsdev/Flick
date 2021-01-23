ac8bb722694efc18dee158a5524a7691b8877ddc
robyn <65057146+robynsdev@users.noreply.github.com>
2021-01-24 01:00:52 +1100

    added Movie def create and show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,33 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show]
   def index
     @movies = Movie.all
     render json: @movies
   end
 
+  def create
+    @movie = Movie.create(movie_params)
+    if @movie.errors.any?
+      render json: @movie.errors, status: :unprocessable_entity
+    else
+      render json: @movie, status: 201
+    end
+  end
+
+  def show
+    render json: @movie
+  end
+  
+
+  
+
+  private
+  def movie_params
+    params.permit(:title, :release_date, :description, :genre, :runtime)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
 
 end
