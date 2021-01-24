d3325fb89e49c3cb0618b5a7a96b75e2ff138f1a
robyn <65057146+robynsdev@users.noreply.github.com>
2021-01-24 17:52:30 +1100

    added User model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_213932) do
+ActiveRecord::Schema.define(version: 2021_01_24_063450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2021_01_21_213932) do
     t.string "description"
     t.string "genre"
     t.integer "run_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
