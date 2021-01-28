8297dbeed71f0e8865df3e26d674603042bffdd8
robyn <65057146+robynsdev@users.noreply.github.com>
2021-01-28 20:59:49 +1100

    changed before_action auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user, except: [:create, :show, :sign_in]
+  before_action :authenticate_user, except: [:create, :profile, :sign_in]
   before_action :set_user, only: [:profile, :update, :delete]
 
   def create
