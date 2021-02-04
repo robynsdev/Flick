c179957f4cb0194eb2d51cc00eea2fd1857a455c
robyn <65057146+robynsdev@users.noreply.github.com>
2021-02-05 00:42:22 +1100

    changed route /profile to get

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     scope '/auth' do
       post '/sign_up', to: 'users#create'
       post '/sign_in', to: 'users#sign_in'
-      post '/profile', to: 'users#profile'
+      get '/profile', to: 'users#profile'
       put '/update', to: 'users#update' 
       delete '/delete', to: 'users#delete' 
     end
