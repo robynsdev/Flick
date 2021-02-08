99d26cbb9b33c1e0fe95cf3391dfe16320d8c552
robyn <65057146+robynsdev@users.noreply.github.com>
2021-02-08 11:32:34 +1100

    added authentication helper and config to run before requests

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -1,1 +1,11 @@
+module AuthenticationHelpers
+  def authenticated_header
+    user = FactoryBot.create(:user)
+    token = Knock::AuthToken.new(payload: {sub: user.id}).token
+    { 'Authorization': "Bearer #{token}" }
+  end
+end
 
+RSpec.configure do |config|
+  config.include AuthenticationHelpers, type: :request
+end
